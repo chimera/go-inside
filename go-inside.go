f9f00048444806e8af9ffecd1fe7cc8e8f334341
Dana Woodman <dana@danawoodman.com>
2013-11-23 17:45:55 -0800

    handle creating a user file if none exists

--- go-inside.go
+++ go-inside.go
@@ -8,8 +8,8 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	// "os"
 	"flag"
+	"os"
 	"strconv"
 )
 
@@ -30,20 +30,48 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 
 func GetUsers() []User {
 
+	// Check if the file does not exist yet.
+	if _, err := os.Stat(*users_file); os.IsNotExist(err) {
+
+		// Let user know we created a user file for them.
+		log.Print("No users JSON file, creating one now: ", *users_file)
+
+		// Create file.
+		file, err := os.Create(*users_file)
+		if err != nil {
+			log.Fatal("Error opening/creating users file: ", err.Error())
+		}
+		defer file.Close()
+
+		// Make the file only readable/writable by the current user.
+		err = os.Chmod(*users_file, 0600)
+		if err != nil {
+			log.Fatal("Could not update permissions to user file: ", err.Error())
+		}
+
+		// Add an empty JSON list to the file so it can be parsed by the JSON marshaller.
+		_, err = file.WriteString("[]")
+		if err != nil {
+			log.Fatal("Could not add empty JSON hash to file: ", err.Error())
+		}
+
+		// Close the file now that we're done with.
+		file.Close()
+	}
+
 	// Read file contents into a list of bytes
 	outputAsBytes, err := ioutil.ReadFile(*users_file)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Error reading file contents: ", err.Error())
 	}
 
 	// Parse the JSON in the file and return a slice of User structs.
 	var output []User
 	err = json.Unmarshal(outputAsBytes, &output)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Error unmarshalling JSON file: ", err.Error())
 	}
 
-	// Available codes to query against.
 	return output
 }
 
@@ -72,7 +100,7 @@ func CheckinHandler(w http.ResponseWriter, r *http.Request) {
 
 		// Code didn't match an authenticated user.
 		w.WriteHeader(http.StatusUnauthorized)
-
+		// TODO: Return a struct converted to JSON.
 		fmt.Fprintf(w, `{ "message": "Bad code there Hass!" }`)
 		log.Print("Invalid code input: ", code)
 		return
@@ -80,6 +108,7 @@ func CheckinHandler(w http.ResponseWriter, r *http.Request) {
 
 	// No code passed in.
 	w.WriteHeader(http.StatusNotAcceptable)
+	// TODO: Return a struct converted to JSON.
 	fmt.Fprintf(w, `{ "message": "Gotta give me a code, bro!" }`)
 	log.Print("Missing code input: ", code)
 	return
@@ -90,6 +119,7 @@ func RedirectHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	flag.Parse()
 
 	// Create the webserver
 	r := mux.NewRouter()

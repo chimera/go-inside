2457896855c9392103a09f240c3233eacb881b2c
Dana Woodman <dana@danawoodman.com>
2013-11-27 13:59:19 -0800

    refactor to be command line only

---
+++ door/door.go
@@ -0,0 +1,91 @@
+package door
+
+import (
+	"fmt"
+	"github.com/chimera/go-inside/users"
+	"log"
+	"rs232"
+	// "time"
+)
+
+type DoorLock struct {
+	Serial         rs232.Port
+	Baud           int
+	SerialPortPath string
+	UsersFile      string
+}
+
+func (d *DoorLock) Connect() {
+	log.Println("Connecting to door lock via serial...")
+
+	// Configure the serial connection.
+	options := rs232.Options{
+		BitRate:  uint32(d.Baud),
+		DataBits: 8,
+		StopBits: 1,
+		Parity:   rs232.PARITY_NONE,
+		Timeout:  0,
+	}
+
+	p, err := rs232.Open(d.SerialPortPath, options)
+	if err != nil {
+		log.Printf("rs232.Open(): %s\n", err)
+		e := err.(*rs232.Error)
+		errType := ""
+		switch e.Code {
+		case rs232.ERR_DEVICE:
+			errType = "ERR_DEVICE"
+		case rs232.ERR_ACCESS:
+			errType = "ERR_ACCESS"
+		case rs232.ERR_PARAMS:
+			errType = "ERR_PARAMS"
+		}
+		log.Fatalf("Failed to connect to serial port with error code: %d (%s)\n", e.Code, errType)
+	}
+
+	log.Printf("Opened serial port %s\n", p.String())
+
+	d.Serial = *p
+}
+
+func (d *DoorLock) Unlock() {
+	log.Println("Unlocking door...")
+	_, err := d.Serial.Write([]byte("1"))
+	if err != nil {
+		log.Fatalf("Could not unlock door, with error: %s", err)
+	}
+	log.Println("Door unlocked!")
+}
+
+func (d *DoorLock) Disconnect() {
+	log.Println("Disconnecting from door lock...")
+	d.Serial.Close()
+}
+
+func (d *DoorLock) Listen() {
+
+	// Listen for incoming RFID codes.
+	for {
+		log.Print("Please input your RFID code for access: ")
+
+		// Check for incoming RFID codes.
+		var code string
+		_, err := fmt.Scan(&code)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// If a code is received, send it to get authenticated.
+		err = users.AuthenticateCode(code, d.UsersFile)
+		if err != nil {
+			log.Println(err.Error())
+		} else {
+			// Log them in if their code is valid.
+			log.Printf("Congrats, your code '%s' is valid, come on in!\n", code)
+			d.Unlock()
+		}
+	}
+
+	// Make sure to disconnect from the door when we're done.
+	defer d.Disconnect()
+}
--- go-inside.go
+++ go-inside.go
@@ -1,138 +1,27 @@
+// +build linux,darwin
+// The main package handles connecting to the door lock and receiving various
+// command line flags, including the JSON users database file, serial port location
+// and the baud rate.
 package main
 
 import (
-	"encoding/json"
 	"flag"
-	"fmt"
-	"github.com/gorilla/mux"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"os"
-	"strconv"
+	"github.com/chimera/go-inside/door"
 )
 
-var users_file = flag.String("users-file", "users.json", "The users JSON file to use.")
-var port = flag.Int("port", 3000, "The port to run on.")
-
-type User struct {
-	Name, Code string
-}
-
-func GetUsers() []User {
-
-	// Check if the file does not exist yet.
-	if _, err := os.Stat(*users_file); os.IsNotExist(err) {
-
-		// Let user know we created a user file for them.
-		log.Print("No users JSON file, creating one now: ", *users_file)
-
-		// Create file.
-		file, err := os.Create(*users_file)
-		if err != nil {
-			log.Fatal("Error opening/creating users file: ", err.Error())
-		}
-		defer file.Close()
-
-		// Make the file only readable/writable by the current user.
-		err = os.Chmod(*users_file, 0600)
-		if err != nil {
-			log.Fatal("Could not update permissions to user file: ", err.Error())
-		}
-
-		// Add an empty JSON list to the file so it can be parsed by the JSON marshaller.
-		_, err = file.WriteString("[]")
-		if err != nil {
-			log.Fatal("Could not add empty JSON hash to file: ", err.Error())
-		}
-
-		// Close the file now that we're done with.
-		file.Close()
-	}
-
-	// Read file contents into a list of bytes
-	outputAsBytes, err := ioutil.ReadFile(*users_file)
-	if err != nil {
-		log.Fatal("Error reading file contents: ", err.Error())
-	}
-
-	// Parse the JSON in the file and return a slice of User structs.
-	var output []User
-	err = json.Unmarshal(outputAsBytes, &output)
-	if err != nil {
-		log.Fatal("Error unmarshalling JSON file: ", err.Error())
-	}
-
-	return output
-}
-
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "public/index.html")
-}
-
-func CheckinHandler(w http.ResponseWriter, r *http.Request) {
-
-	// Make sure to return JSON.
-	// w.Header().Set("Content-Type", "applicaiton/json")
-
-	// Fetch the list of authenticated users.
-	users := GetUsers()
-
-	// Get the code from the request.
-	code := r.FormValue("code")
-
-	// See if the code matches our list of authenticated users.
-	if code != "" {
-		log.Print("Received RFID code: ", code)
-		for _, user := range users {
-			if code == user.Code {
-				w.WriteHeader(http.StatusOK)
-				// TODO: Return a struct converted to JSON.
-				fmt.Fprintf(w, `{ "message": "Nice one %s!" }`, user.Name)
-				return
-			}
-		}
-
-		// Code didn't match an authenticated user.
-		w.WriteHeader(http.StatusUnauthorized)
-		// TODO: Return a struct converted to JSON.
-		fmt.Fprintf(w, `{ "message": "Bad code there Hass!" }`)
-		log.Print("Invalid code input: ", code)
-		return
-	}
-
-	// No code passed in.
-	w.WriteHeader(http.StatusNotAcceptable)
-	// TODO: Return a struct converted to JSON.
-	fmt.Fprintf(w, `{ "message": "Gotta give me a code, bro!" }`)
-	log.Print("Missing code input: ", code)
-	return
-}
-
-func RedirectHandler(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "/", http.StatusSeeOther)
-}
+// Available command line flags with sane-ish defaults.
+var users_file = flag.String("db", "users.json", "The users JSON file to use.")
+var port_path = flag.String("port", "/dev/tty.usbmodem621", "The serial port that the Arduino is running on.")
+var baud = flag.Int("baud", 19200, "The baudrate to connect to the serial port with.")
 
 func main() {
+	// Parse any command line flags.
 	flag.Parse()
 
-	// Create the webserver
-	r := mux.NewRouter()
+	// Connect to the door lock.
+	door := &door.DoorLock{Baud: *baud, SerialPortPath: *port_path, UsersFile: *users_file}
+	door.Connect()
 
-	// Handle the checkin form.
-	r.HandleFunc("/", HomeHandler)
-
-	// Redirect people if they get to checking via GET.
-	r.HandleFunc("/checkin", RedirectHandler).Methods("GET")
-
-	// Listen for submissions of the checkin form
-	r.HandleFunc("/checkin", CheckinHandler).Methods("POST")
-
-	// Handle the routes.
-	http.Handle("/", r)
-
-	// Run the server.
-	p := ":" + strconv.Itoa(*port)
-	log.Print("Starting server on port", p)
-	http.ListenAndServe(p, nil)
+	// Handle inputting of user RFID codes
+	door.Listen()
 }
